b1564636d6b9cbd5453fbb6ddec576ad839e167a
Clem White <clemw@hotmail.com>
2024-11-08 20:37:12 +1300

    Fix database configuration with HikariCP

--- src/main/java/nz/clemwhite/blogsite/config/DatabaseConfig.java
+++ src/main/java/nz/clemwhite/blogsite/config/DatabaseConfig.java
@@ -1,5 +1,6 @@
 package nz.clemwhite.blogsite.config;
 
+import com.zaxxer.hikari.HikariDataSource;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.jdbc.DataSourceBuilder;
 import org.springframework.context.annotation.Bean;
@@ -8,16 +9,25 @@ import javax.sql.DataSource;
 
 @Configuration
 public class DatabaseConfig {
+
     @Value("${DATABASE_URL}")
     private String databaseUrl;
+
+    @Value("${POSTGRES_USER}")
+    private String username;
+
+    @Value("${POSTGRES_PASSWORD}")
+    private String password;
 
     @Bean
     public DataSource dataSource() {
         String jdbcUrl = "jdbc:" + databaseUrl.replace("postgres://", "postgresql://");
-        return DataSourceBuilder.create()
-                .url(jdbcUrl)
-                .username("${POSTGRES_USER}")
-                .password("${POSTGRES_PASSWORD}")
-                .build();
+
+        HikariDataSource dataSource = new HikariDataSource();
+        dataSource.setJdbcUrl(jdbcUrl);
+        dataSource.setUsername(username);
+        dataSource.setPassword(password);
+
+        return dataSource;
     }
 }
